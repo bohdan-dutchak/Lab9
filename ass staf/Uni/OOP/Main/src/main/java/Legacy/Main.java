d7a0a2953de801076af0fd89ea0743572892edcc
Bohdan <bogdandutchak02@gmail.com>
2021-11-14 22:27:17 +0200

    Added tests and configs

---
+++ ass staf/Uni/OOP/Main/src/main/java/Legacy/Main.java
@@ -0,0 +1,14 @@
+package Legacy;
+
+import authorization.Authorization;
+import db.DataBase;
+
+public class Main {
+    public static void main(String[] args) {
+        DataBase db = new DataBase();
+        Authorization authorization = new Authorization();
+        if (authorization.authorize(db)) {
+            ReportBuilder br = new ReportBuilder(db);
+        }
+    }
+}
